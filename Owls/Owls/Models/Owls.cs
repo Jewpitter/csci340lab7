10f006b90ad5efe162f5544df82232f789ed7417
Josh <thomeczekje@hendrix.edu>
2021-10-11 23:20:23 -0500

    see below

--- Owls/Owls/Models/Owls.cs
+++ Owls/Owls/Models/Owls.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Owls.Models
 {
     public class Owls
     {
-        public Owls()
-        {
-        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Age { get; set; }
+        public string Species { get; set; }
+        public decimal Height { get; set; }
     }
 }
